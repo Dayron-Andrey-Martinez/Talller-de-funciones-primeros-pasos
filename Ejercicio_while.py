b09edf7d9f5b42f107be372c45889b781a1b095d
Dayron-Andrey-Martinez <dayronandreymartinez@gmail.com>
2025-12-01 08:06:31 -0500

    Add files via upload
    este código es un ciclo para saber si es par e impar.

---
+++ Ejercicio_while.py
@@ -0,0 +1,27 @@
+#*********zona de funciones***********
+
+def tomar_numero():
+    numero_digitado= int(input("dijite un numero: "))
+    return numero_digitado
+
+def identificar_numero(numero):
+    if numero%2==0:
+        return "El numero es par  "
+    else:
+        return "El numero es impar "
+
+def enviar_mensaje(numero_digitado, numero):
+    mensaje= f"el numero {numero_digitado} es {numero}"
+    return mensaje
+
+def imprimir_mensaje(mensaje):
+    print(mensaje)
+    
+#************codigo principal**********
+numero=1;
+while numero!=0:
+     dato_numero= tomar_numero()
+     tipo_numero= identificar_numero(dato_numero)
+     dato_mensaje=enviar_mensaje(dato_numero, tipo_numero)
+     imprimir_mensaje(dato_mensaje)
+     print("finalizo el programa ")
